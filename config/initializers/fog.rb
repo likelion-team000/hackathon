252e5ac0cb5cac4fb8ba3c4f42042704d7ebdc03
Jessica Song <jpark2973@gmail.com>
2017-04-01 15:12:23 -0700

    add carrierwave

---
+++ config/initializers/fog.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     'AKIAIP2JXZVLW4DJQFOA',                        # required
+    aws_secret_access_key: '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',                        # required
+    # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+    # host:                  's3.example.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'hackthon-likelion'                          # required
+  config.fog_public     = true                                        # optional, defaults to true
+  # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@
 ActiveRecord::Schema.define(version: 20170401200705) do
 
   create_table "posts", force: :cascade do |t|
+    t.string   "userId"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
