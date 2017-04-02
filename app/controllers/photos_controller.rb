8e36e5160c49c4e21876e306f835e4a774c66292
Yong Bum Yoo <yongy0221@gmail.com>
2017-04-01 21:48:20 -0700

    added unlike button

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,13 @@
 class PhotosController < ApplicationController
+	before_action :authenticate_user!, only: [:upvote]
+
 	def upvote
 		@photo = Photo.find(params[:id])
-		@photo.upvote_by current_user
+		if !current_user.liked? @photo
+			@photo.liked_by current_user
+		elsif current_user.liked? @photo
+			@photo.unliked_by current_user
+		end
   		redirect_to :back
 	end
 
